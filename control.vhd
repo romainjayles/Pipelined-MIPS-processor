d4742809f094430026f003ad5b6b9c96a6c575a5
Romain Jayles <romain@jayles.net>
2015-10-27 17:00:51 +0100

    adding control

---
+++ control.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:58:19 10/27/2015 
+-- Design Name: 
+-- Module Name:    control - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity control is
+
+port(
+
+end control;
+
+architecture Behavioral of control is
+
+begin
+
+
+end Behavioral;
+
