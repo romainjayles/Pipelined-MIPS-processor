ffac41bb84743374376bd6c22efcd5cbaaf9831a
Romain Jayles <romain@jayles.net>
2015-11-10 09:25:26 +0100

    some changes

--- testbench/tb_MIPSProcessor_stall.vhd
+++ testbench/tb_MIPSProcessor_stall.vhd
@@ -111,14 +111,10 @@ DataMem:			entity work.DualPortMem port map (
 		end WriteInstructionWord;
 		
 		procedure FillInstructionMemory is
-			constant TEST_INSTRS : integer := 95;
+			constant TEST_INSTRS : integer := 67;
 			type InstrData is array (0 to TEST_INSTRS-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
 			variable TestInstrData : InstrData := (
-				X"8C010001", --lw $1, 1($0)		/$1 =  2
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
+				X"8C010001", --lw $1, 1($0)		/$1 =  2	
 				X"8C020002", --lw $2, 2($0)		/$2 = 10
 				X"00000020",
 				X"00000020",
@@ -130,17 +126,9 @@ DataMem:			entity work.DualPortMem port map (
 				X"00000020",
 				X"00000020",
 				X"AC030005", --sw $3, 5($0)		/Saving value 12 on address 5	
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"10000005", --beq $0, $0, 2		/Jumping to adress +2 = 8	
+				X"10000003", --beq $0, $0, 2		/Jumping to adress +2 = 8	
 				X"AC030003", --sw $3, 3($0)      /SKIPPED (Saving value 12 on address 3)			
-				X"AC030003", --sw $3, 4($0)		/SKIPPED	(Saving value 12 on address 4)
-				X"AC030003",
-				X"AC030004",
-				X"AC030003",
-				X"AC030003",
+				X"AC030004", --sw $3, 4($0)		/SKIPPED	(Saving value 12 on address 4)
 				X"AC030006", --sw $3, 6($0)		/Saving value 12 on address 6	
 				X"AC030007", --sw $3, 7($0)		/Saving value 12 on address 7	
 				X"3C030006", --lui $3, 6			/$3 = 6 * 2^16 = 393216 = 0x60000
@@ -148,14 +136,9 @@ DataMem:			entity work.DualPortMem port map (
 				X"00000020",
 				X"00000020",
 				X"00000020",
+				X"00000020",
 				X"AC030008", --sw $3, 8($0)		/Saving value 0x60000 on address 8
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
 				X"00231820", --add $3, $1, $3		/$3 = 393218 = 0x60002
-				X"00000020",
 				X"00000020",
 				X"00000020",
 				X"00000020",
@@ -167,17 +150,16 @@ DataMem:			entity work.DualPortMem port map (
 				X"00000020",
 				X"00000020",
 				X"00000020",
-				X"00000020",				
 				X"AC13000C", --sw $19, 12($0)		/Saving 1 on address 12	
-				X"08000013", --j 19					/jump to 19
-				X"AC030001", --sw $3, 1($0)		/SKIPPED (Saving 0x60002 on address 1)	
-				X"1000FFFD", --beq $0, $0, -3		/SKIPPED (Branch back three steps)	
-				X"00622022", --sub $4, $3, $2		/$4 = 0x5FFF8
+				--X"0800002a", --j 19					/jump to 19
+				--X"AC030001", --sw $3, 1($0)		/SKIPPED (Saving 0x60002 on address 1)	
+				--X"1000FFFD", --beq $0, $0, -3		/SKIPPED (Branch back three steps)	
+				X"00622022", --sub $4, $3, $2		/$4 = 0x5FFF8 
 				X"00000020",
 				X"00000020",
 				X"00000020",
 				X"00000020",
-				X"00000020",				
+				X"00000020",
 				X"00822022", --sub $4, $4, $2		/$4 = 0x5FFEE
 				X"00000020",
 				X"00000020",
@@ -190,15 +172,8 @@ DataMem:			entity work.DualPortMem port map (
 				X"00000020",
 				X"00000020",
 				X"00000020",
-				X"00000020",
-				X"00432024", --and $4, $2, $3		/$4 = 1000 = 8
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
-				X"00000020",
+				X"00432024", --and $4, $2, $3		/$4 = 1000 = 8	
 				X"00432825", --or $5, $2, $3		/$5 = 1110 = 14
-				X"00000020",
 				X"00000020",
 				X"00000020",
 				X"00000020",
